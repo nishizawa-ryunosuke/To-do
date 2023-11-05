a66351dc7360f9f692c32851dddd6760543977ab
nishizawa-ryunosuke <ryukuu0919@gmail.com>
2023-11-05 18:41:25 +0900

    完成

--- todo-app/src/app/page.tsx
+++ todo-app/src/app/page.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import TodoList from '../components/TodoList';
 
-const Home: React.FC = () => {
+// Todoアプリの中身
+const Home = () => {
     return (
         <div className="container mx-auto p-8 text-center max-w-2xl">
             <h1 className="text-2xl mb-4">Todo App</h1>
--- todo-app/src/components/TodoItem.tsx
+++ todo-app/src/components/TodoItem.tsx
@@ -1,16 +1,14 @@
+// 必要なライブラリをインポート
 import React from 'react';
 
-type TodoItemProps = {
-    task: string;
-    toggleCompletion: () => void;
-};
-
-const TodoItem: React.FC<TodoItemProps> = ({ task, toggleCompletion }) => {
+const TodoItem = (props: { task: string; toggleCompletion: () => void; }) => { // ←注意ポイント④
     return (
         <div className="flex justify-between p-2 border-b bg-white mb-5">
-            <span className="flex-1">{task}</span>
-            <button onClick={toggleCompletion} className="text-xl">
-                ✗
+            {/* タスクテキストを表示 */}
+            <span className="flex-1">{props.task}</span>
+            {/* toggleCompletion関数をトリガーするボタン */}
+            <button onClick={props.toggleCompletion} className="text-xl">
+                ✓
             </button>
         </div>
     );
